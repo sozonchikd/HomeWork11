bb0e4d3062355a3e0b15d822f3af3d6b373fbe4f
Sozonchik <den.sozon@mail.ru>
2022-10-21 14:03:22 +0500

    Сделал задание 3

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,11 @@ public class Main {
         int clientOS = 0;
         requiredApplicationVersion(clientOS, currentYear);
 
+        // Задание 3.
+        System.out.println("Задание 3");
+        int deliveryDistance = 95;
+        int days = deliveryPeriod(deliveryDistance);
+        System.out.println("Потребуется дней: " + days);
     }
 
     // Метод задания 1.
@@ -21,26 +26,38 @@ public class Main {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год високосный!");
         } else {
-            System.out.println(year + " год не високосный");
+            System.out.println(year + " год не високосный.");
         }
     }
 
+    // Метод задания 2.
     public static void requiredApplicationVersion(int operatingSystem, int requiredYear) {
         if (operatingSystem == 0) {
             if (requiredYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
             } else {
-                System.out.println("Установите приложение для iOS по ссылке");
+                System.out.println("Установите приложение для iOS по ссылке.");
             }
         }
         if (operatingSystem == 1) {
             if (requiredYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android  по ссылке");
+                System.out.println("Установите облегченную версию приложения для Android  по ссылке.");
             } else {
-                System.out.println("Установите приложение для Android по ссылке");
+                System.out.println("Установите приложение для Android по ссылке.");
             }
         }
     }
 
+    // Метод задания 3.
+    public static int deliveryPeriod(int distance) {
+        int deliveryDays = 1;
+        if (distance > 20) {
+            deliveryDays++;
+        }
+        if (distance > 60) {
+            deliveryDays++;
+        }
+        return deliveryDays;
+    }
 
 }
